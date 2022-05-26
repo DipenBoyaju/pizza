cce2cc6764c683678a5fa22458438718efa3cfbd
DipenBoyaju <dipendraboyaju222@gmail.com>
2022-05-26 12:23:38 +0545

    demo1

---
+++ include/footer.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Footer</title>
+</head>
+<body>
+<footer class="footer solid-bg-two">
+	<div class="tp-footer">
+		<div class="newsletter">
+				<form>
+					<h3>SUBSCRIBE FOR OUR NEWSLETTER</h3>
+					<input type="text" name="" placeholder="Enter Your Email">
+					<a href=""><button type="submit">Subscribe</button></a>
+				</form>
+			</div>
+		<div class="container">
+			<div class="footer-mid">
+				<div class="row">
+					<div class="col-md-3">
+						<h4>Address</h4>
+						<p>Libali, Bhaktapur - 8, Nepal</p>
+					</div>
+					<div class="col-md-3">
+						<h4>Contact</h4>
+						<p>info@mysite.com<br>Tel: 123-456-7890</p>
+					</div>
+					<div class="col-md-3">
+						<h4>Hours</h4>
+						<p>OPEN DAILY<br>10AM-10PM</p>
+					</div>
+					<div class="col-md-3">
+						<h4>Social Media</h4>
+						<i class="fab fa-facebook"></i>
+						
+					</div>
+				</div>
+			</div>
+			<div class="footer-bottom">
+				<p>Copyright © 2021 Dipen Boyaju. All Rights Reserved.</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>header</title>
+
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href="https://fonts.googleapis.com/css2?family=PT+Sans+Narrow:wght@400;700&family=Pacifico&display=swap" rel="stylesheet">
+</head>
+<body>
+<section class="header">
+	<div class="container">
+		<div class="logo">
+			<a href="index.php">Pizza Chen</a>
+		</div>
+		<nav class="navbar">
+			<ul>
+				<a href="index.php"><li>Home</li></a>
+				<a href="menu.php"><li>Menu</li></a>
+				<a href="contact.php"><li>Contact</li></a>
+				<span class="log"><li><a href="">Order Online</a></li></span>
+			</ul>
+		</nav>
+	</div>
+</section>
+</body>
+</html>
